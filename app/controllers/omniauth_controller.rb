a45dca037cc4bec7c96ed9af55c1e7c96e073ad0
NikshepAV <nikshep.av@gmail.com>
2018-07-11 09:48:02 +0530

    Fix flash messages

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -8,13 +8,13 @@ class OmniauthController < ApplicationController
         flash[:notice] = "Signed in successfully."
       end
     else
-      flash[:notice] = 'There was a problem signing you in through Google. Please register or try signing in later.'
+      flash[:alert] = 'There was a problem signing you in through Google. Please register or try signing in later.'
       redirect_to new_user_registration_url
     end
   end
 
   def failure
-    flash[:notice] = 'There was a problem signing you in. Please register or try signing in later.'
+    flash[:alert] = 'There was a problem signing you in. Please register or try signing in later.'
     redirect_to new_user_registration_url
   end
 end
