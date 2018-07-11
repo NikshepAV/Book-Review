d5ce92c912374d14b817e6d420274d1015a1c8f2
NikshepAV <nikshep.av@gmail.com>
2018-07-11 12:45:19 +0530

    Add name columns in user db

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
       user.email = provider_data.info.email
+      user.first_name = provider_data.info.first_name
+      user.last_name = provider_data.info.last_name
       user.password = Devise.friendly_token[0, 20]
     end
   end
