840e2289a57bfd6e191e11cfa2e6972dff84de18
NikshepAV <nikshep.av@gmail.com>
2018-07-11 18:04:36 +0530

    Add basic profile functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: 'omniauth'}
+  resources :users, :only => [:show]
   resources :books do
     resources :reviews
   end
