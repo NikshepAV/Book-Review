fc4be31186c9d0a570846816311ebaf0bb8c27cf
NikshepAV <nikshep.av@gmail.com>
2018-07-12 12:08:14 +0530

    Deployment fix commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,8 @@ class User < ApplicationRecord
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
       user.email = provider_data.info.email
       # user.name = provider_data.info.name
-      # user.first_name = provider_data.info.first_name
-      # user.last_name = provider_data.info.last_name
+      user.first_name = provider_data.info.first_name
+      user.last_name = provider_data.info.last_name
       user.image = provider_data.info.image
       user.password = Devise.friendly_token[0, 20]
     end
