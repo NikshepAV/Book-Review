5af4422183f4b2a8353bddbda2d73a0dad946232
NikshepAV <nikshep.av@gmail.com>
2018-07-12 17:52:40 +0530

    Make layout changes

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,14 +1,19 @@
 class OmniauthController < ApplicationController
   # google callback
   def google_oauth2
-    @user = User.create_from_provider_data(request.env['omniauth.auth'])
-    if @user.persisted?
-      sign_in_and_redirect @user
-      if is_navigational_format?
-        flash[:notice] = "Signed in successfully."
+    if request.env['omniauth.auth'].info.email.match  (/^[A-Za-z0-9._%+-]+@mavenhive.in$/)
+      @user = User.create_from_provider_data(request.env['omniauth.auth'])
+      if @user.persisted?
+        sign_in_and_redirect @user
+        if is_navigational_format?
+          flash[:notice] = "Signed in successfully."
+        end
+      else
+        flash[:alert] = 'There was a problem signing you in through Google. Please register or try signing in later.'
+        redirect_to new_user_registration_url
       end
     else
-      flash[:alert] = 'There was a problem signing you in through Google. Please register or try signing in later.'
+      flash[:alert] = 'You can log in only using a Mavenhive account.'
       redirect_to new_user_registration_url
     end
   end
---
+++ db/migrate/20180712102551_add_quantity_to_books.rb
@@ -0,0 +1,6 @@
+class AddQuantityToBooks < ActiveRecord::Migration[5.1]
+  def change
+    add_column :books, :total, :integer, :default => 1
+    add_column :books, :available, :integer, :default => 1
+  end
+end
