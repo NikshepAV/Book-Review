be15f665506c9a1b9e95a12d1976fba4c3846e54
NikshepAV <nikshep.av@gmail.com>
2018-07-16 12:31:42 +0530

    Model fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180716065243) do
+ActiveRecord::Schema.define(version: 20180716070039) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20180716065243) do
     t.string "book_img_content_type"
     t.integer "book_img_file_size"
     t.datetime "book_img_updated_at"
+    t.integer "total", default: 1
+    t.integer "available", default: 1
   end
 
   create_table "categories", force: :cascade do |t|
