b88b062c3fbf95656f7593f41ef8a3e753e46fde
NikshepAV <nikshep.av@gmail.com>
2018-07-16 12:14:58 +0530

    Model fixes

--- db/migrate/20180713052410_add_quantity_to_books.rb
+++ db/migrate/20180713052410_add_quantity_to_books.rb
@@ -1,6 +1,6 @@
 class AddQuantityToBooks < ActiveRecord::Migration[5.1]
   def change
-    add_column :books, :total, :integer, default: 1
-    add_column :books, :available, :integer, default: 1
+    add_column :books, :total, :integer, :default => 1, :nil => false
+    add_column :books, :available, :integer, :default => 1, :nil => false
   end
 end
